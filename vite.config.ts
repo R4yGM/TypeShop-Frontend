b3d1c910efca57e3101fd8937e622896dd0bff6d
R4yan <yessou.rayan@gmail.com>
2023-11-23 23:41:39 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react({
+    // Add this line
+    include: "**/*.tsx",
+  })],
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
